98997474620a94b091efc62c8998d3a27f7833e7
Aritra <aritrabiswas9788@gmail.com>
2024-05-12 01:55:30 +0530

    Dashboard Fragment completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -163,6 +163,10 @@ dependencies {
     implementation ("org.tensorflow:tensorflow-lite-metadata:0.1.0")
 
 
+    implementation("com.mapbox.maps:android:10.16.0")
+    implementation ("com.mapbox.search:discover:1.2.0")
+    implementation("com.google.ai.client.generativeai:generativeai:0.4.0")
+
     implementation ("com.firebase:geofire-android:3.2.0")
 
     // GeoFire utililty functions for Cloud Firestore users who
