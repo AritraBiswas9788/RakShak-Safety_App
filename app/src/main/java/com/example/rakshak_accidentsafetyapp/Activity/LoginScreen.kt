1a0a90abdf487723c746c40db646c9c896bee325
jainjabhi05 <jainjabhi05@gmail.com>
2024-05-11 18:12:19 +0530

    login

---
+++ app/src/main/java/com/example/rakshak_accidentsafetyapp/Activity/LoginScreen.kt
@@ -0,0 +1,13 @@
+package com.example.rakshak_accidentsafetyapp.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rakshak_accidentsafetyapp.R
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+
+    }
+}
